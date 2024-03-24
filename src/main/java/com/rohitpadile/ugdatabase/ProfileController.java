ce20aedf2b778fb989de363b2c6793a2412b1085
Rohit Padile <rohitpadile24@gmail.com>
2024-03-24 11:55:43 +0530

    Dialog (Alert) for invalid Mis is working fine

--- src/main/java/com/rohitpadile/ugdatabase/ProfileController.java
+++ src/main/java/com/rohitpadile/ugdatabase/ProfileController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -148,6 +149,12 @@ public class ProfileController {
                 stage.show();
             } else {
                 //open a dialog which says student not found.
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("Invalid Mis");
+                alert.setHeaderText(null);
+                alert.setContentText("Invalid Mis");
+
+                alert.showAndWait();
             }
 
         } catch (IOException e) {
